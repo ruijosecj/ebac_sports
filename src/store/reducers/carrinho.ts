5738f41a6ec3454d2be0b92eae9077ad45511137
ruijosecj <ruijosecj@gmail.com>
2023-09-06 15:57:40 -0300

    projeto atualizado com redux

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,29 @@
+import { Produto } from '../../App'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
